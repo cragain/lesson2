178ee541a77dc7a65d9e3231231d8ec3ac2f6f87
TPPcragain <chris.ragain@taxplannerpro.com>
2016-07-13 16:08:37 +0000

    little bit of refactor

---
+++ lesson2/calculator.rb
@@ -0,0 +1,23 @@
+puts 'Please provide the first number...'
+
+x = gets.chomp.to_f
+
+puts 'Please provide the second number...'
+
+y = gets.chomp.to_f
+
+puts 'What kind of math do you need? Type a) addition b) subtraction c) multiplication d) division'
+
+z = gets.chomp
+
+if z == 'a'
+  answer = x + y
+elsif z == 'b'
+  answer = x - y 
+elsif z == 'c'
+  answer = x * y 
+else answer = x / y 
+  
+end 
+
+puts answer
