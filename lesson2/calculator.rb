308a7a524b48b99d5b7d9d76f24e53ffe6f91002
TPPcragain <chris.ragain@taxplannerpro.com>
2016-07-13 16:49:34 +0000

    added validation to number and operator

--- lesson2/calculator.rb
+++ lesson2/calculator.rb
@@ -1,23 +1,65 @@
-puts 'Please provide the first number...'
+def prompt(message)
+  puts("=> #{message}")
+end
 
-x = gets.chomp.to_f
+def right_num?(number)
+  number.to_i != 0
+end
 
-puts 'Please provide the second number...'
+def right_math?(operator)
+  options = ['a', 'b', 'c', 'd'] 
+  options.include? (operator)
+end
 
-y = gets.chomp.to_f
+x = ''
+y = ''
+z = ''
 
-puts 'What kind of math do you need? Type a) addition b) subtraction c) multiplication d) division'
+loop do
+  prompt("Please provide the first number...")
+  x = gets.chomp.to_f
 
-z = gets.chomp
+  if right_num?(x)
+    break
+  else
+    prompt("that is not a valid number, try again..")
+  end
 
-if z == 'a'
-  answer = x + y
-elsif z == 'b'
-  answer = x - y 
-elsif z == 'c'
-  answer = x * y 
-else answer = x / y 
+end
+
+loop do
+  prompt("Please provide the second number...")
+  y = gets.chomp.to_f
   
-end 
+  if right_num?(y)
+    break
+  else
+    prompt("that is not a valid number, try again...")
+  end
+end
 
-puts answer
+
+loop do
+  prompt("What kind of math do you need? Type a) addition b) subtraction c) multiplication d) division")
+  z = gets.chomp
+  
+  if right_math?(z)
+    break
+  else
+    prompt("That is not one of the options, choose a, b, c, or d")
+  end
+end
+
+
+answer = case z 
+         when 'a'
+           x + y 
+         when 'b'
+           x - y 
+         when 'c'
+           x * y 
+         when 'd'
+           x / y 
+end
+
+prompt(answer)
